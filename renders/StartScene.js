2ec7b3d7958a7794e6c5fce682b0cdf750d17edc
LazyNora <lazynora@outlook.com>
2024-11-29 14:17:29 +0700

    fix...

--- renders/StartScene.js
+++ renders/StartScene.js
@@ -37,36 +37,42 @@ export default class StartScene extends Phaser.Scene {
 		this.fontStyle = { fontSize: "30px", fill: "#000", fontFamily: "Nunito" };
 		this.cameras.main.setBackgroundColor("#ffffff");
 
-	
-		const menu = this.add.text(450, 100, "MENU ", {
-			...this.fontStyle,
-			fontSize: '50px',
-			fontWeight: 'bold',
-			fontStyle: 'italic',
-			fill: "#AE445A"
-		}).setOrigin(0.5, 0.5).setVisible(true);
+		const menu = this.add
+			.text(450, 100, "MENU ", {
+				...this.fontStyle,
+				fontSize: "50px",
+				fontWeight: "bold",
+				fontStyle: "italic",
+				fill: "#AE445A",
+			})
+			.setOrigin(0.5, 0.5)
+			.setVisible(true);
 
-		const algorithm = this.add.text(450, 100, "ALGOTHIRMS", {
-			...this.fontStyle,
-			fontSize: '50px',
-			fontWeight: 'bold',
-			fontStyle: 'italic',
-			fill: "#AE445A"
-		}).setOrigin(0.5, 0.5).setVisible(false);
+		const algorithm = this.add
+			.text(450, 100, "ALGOTHIRMS", {
+				...this.fontStyle,
+				fontSize: "50px",
+				fontWeight: "bold",
+				fontStyle: "italic",
+				fill: "#AE445A",
+			})
+			.setOrigin(0.5, 0.5)
+			.setVisible(false);
 
-
-		const level = this.add.text(450, 100, "LEVEL", {
-			...this.fontStyle,
-			fontSize: '50px',
-			fontWeight: 'bold',
-			fontStyle: 'italic',
-			fill: "#AE445A"
-		}).setOrigin(0.5, 0.5).setVisible(false);
-
+		const level = this.add
+			.text(450, 100, "LEVEL", {
+				...this.fontStyle,
+				fontSize: "50px",
+				fontWeight: "bold",
+				fontStyle: "italic",
+				fill: "#AE445A",
+			})
+			.setOrigin(0.5, 0.5)
+			.setVisible(false);
 
 		const singleGraphics = this.add.graphics();
 		singleGraphics.lineStyle(2, 0x000000, 1); // Set border color to black and thickness to 2px
-		singleGraphics.fillStyle(0x3DD1E7, 1); // Set background color to red
+		singleGraphics.fillStyle(0x3dd1e7, 1); // Set background color to red
 		singleGraphics.strokeRoundedRect(350, 185, 200, 50, 10); // Draw rounded rectangle border with border radius 10px
 		singleGraphics.fillRoundedRect(350, 185, 200, 50, 10); // Fill rounded rectangle with border radius 10px
 		singleGraphics.setAlpha(0); // Initially hide the border by setting alpha to 0
@@ -76,16 +82,15 @@ export default class StartScene extends Phaser.Scene {
 				fontSize: "30px",
 				fill: "#000",
 				fontFamily: "Nunito",
-				fontWeight: '700',
-				align: 'center',
+				fontWeight: "700",
 			})
 			.setOrigin(0.5, 0.5)
 			.setInteractive({ useHandCursor: true })
 			.on("pointerdown", () => {
 				single.setVisible(false);
 				multi.setVisible(false);
-				minimax.setVisible(true).setAlpha(1); // Set minimax to visible and fully opaque
-				alphaBeta.setVisible(true).setAlpha(0.5); // Set alphaBeta to visible and semi-transparent
+				minimax.setVisible(true);
+				alphaBeta.setVisible(true);
 				back.setVisible(true); // Show the back button
 				backGraphics.setAlpha(1); // Show the back button background
 				intro.setVisible(false);
@@ -99,10 +104,10 @@ export default class StartScene extends Phaser.Scene {
 					targets: singleGraphics,
 					alpha: 1,
 					duration: 500, // Duration of the fade-in effect in milliseconds
-					ease: 'Power2',
+					ease: "Power2",
 					onComplete: () => {
 						singleGraphics.setVisible(true); // Ensure visibility is set to true after fade-in
-					}
+					},
 				});
 			})
 			.on("pointerout", () => {
@@ -110,16 +115,16 @@ export default class StartScene extends Phaser.Scene {
 					targets: singleGraphics,
 					alpha: 0,
 					duration: 500, // Duration of the fade-out effect in milliseconds
-					ease: 'Power2',
+					ease: "Power2",
 					onComplete: () => {
 						singleGraphics.setVisible(false); // Ensure visibility is set to false after fade-out
-					}
+					},
 				});
 			});
 
 		const multiGraphics = this.add.graphics();
 		multiGraphics.lineStyle(2, 0x000000, 1); // Set border color to black and thickness to 2px
-		multiGraphics.fillStyle(0x3DD1E7, 1); // Set background color to red
+		multiGraphics.fillStyle(0x3dd1e7, 1); // Set background color to red
 		multiGraphics.strokeRoundedRect(350, 280, 200, 50, 10); // Draw rounded rectangle border with border radius 10px
 		multiGraphics.fillRoundedRect(350, 280, 200, 50, 10); // Fill rounded rectangle with border radius 10px
 		multiGraphics.setAlpha(0); // Initially hide the border by setting alpha to 0
@@ -129,8 +134,7 @@ export default class StartScene extends Phaser.Scene {
 				fontSize: "30px",
 				fill: "#000",
 				fontFamily: "Nunito",
-				fontWeight: '700',
-				align: 'center',
+				fontWeight: "700",
 			})
 			.setOrigin(0.5, 0.5)
 			.setInteractive({ useHandCursor: true })
@@ -143,7 +147,7 @@ export default class StartScene extends Phaser.Scene {
 					targets: multiGraphics,
 					alpha: 1,
 					duration: 500, // Duration of the fade-in effect in milliseconds
-					ease: 'Power2'
+					ease: "Power2",
 				});
 			})
 			.on("pointerout", () => {
@@ -151,30 +155,33 @@ export default class StartScene extends Phaser.Scene {
 					targets: multiGraphics,
 					alpha: 0,
 					duration: 500, // Duration of the fade-out effect in milliseconds
-					ease: 'Power2'
+					ease: "Power2",
 				});
 			});
 
-		const intro = this.add.text(450, 400, "Chơi nhiều sẽ hay! Enjoy ", {
-			fontFamily: 'Nunito',
-			fontSize: '40px',
-			color: 'red',
-			fontStyle: 'italic',
-			fill:"#AE445A"
-		}).setOrigin(0.5, 0.5);
+		const intro = this.add
+			.text(450, 400, "Chơi nhiều sẽ hay! Enjoy ", {
+				fontFamily: "Nunito",
+				fontSize: "40px",
+				color: "red",
+				fontStyle: "italic",
+				fill: "#AE445A",
+			})
+			.setOrigin(0.5, 0.5);
 
-		const heart = this.add.text(700, 400, "❤  ", {
-			fontFamily: 'Nunito',
-			fontSize: '40px',
-			color: 'red',
-			fontStyle: 'italic',
-			fill: "#AE445A"
-
-		}).setOrigin(0.5, 0.5);
+		const heart = this.add
+			.text(700, 400, "❤  ", {
+				fontFamily: "Nunito",
+				fontSize: "40px",
+				color: "red",
+				fontStyle: "italic",
+				fill: "#AE445A",
+			})
+			.setOrigin(0.5, 0.5);
 
 		const minimaxGraphics = this.add.graphics();
 		minimaxGraphics.lineStyle(2, 0x000000, 1); // Set border color to black and thickness to 2px
-		minimaxGraphics.fillStyle(0x3DD1E7, 1); // Set background color to red
+		minimaxGraphics.fillStyle(0x3dd1e7, 1); // Set background color to red
 		minimaxGraphics.strokeRoundedRect(350, 185, 200, 50, 10); // Draw rounded rectangle border with border radius 10px
 		minimaxGraphics.fillRoundedRect(350, 185, 200, 50, 10); // Fill rounded rectangle with border radius 10px
 		minimaxGraphics.setAlpha(0); // Initially hide the border by setting alpha to 0
@@ -199,7 +206,7 @@ export default class StartScene extends Phaser.Scene {
 					targets: minimaxGraphics,
 					alpha: 1,
 					duration: 500, // Duration of the fade-in effect in milliseconds
-					ease: 'Power2'
+					ease: "Power2",
 				});
 			})
 			.on("pointerout", () => {
@@ -207,23 +214,20 @@ export default class StartScene extends Phaser.Scene {
 					targets: minimaxGraphics,
 					alpha: 0,
 					duration: 500, // Duration of the fade-out effect in milliseconds
-					ease: 'Power2'
+					ease: "Power2",
 				});
 			})
 			.setVisible(false);
 
 		const alphaBetaGraphics = this.add.graphics();
 		alphaBetaGraphics.lineStyle(2, 0x000000, 1); // Set border color to black and thickness to 2px
-		alphaBetaGraphics.fillStyle(0x3DD1E7, 1); // Set background color 
+		alphaBetaGraphics.fillStyle(0x3dd1e7, 1); // Set background color
 		alphaBetaGraphics.strokeRoundedRect(350, 280, 200, 50, 10); // Draw rounded rectangle border with border radius 10px
 		alphaBetaGraphics.fillRoundedRect(350, 280, 200, 50, 10); // Fill rounded rectangle with border radius 10px
 		alphaBetaGraphics.setAlpha(0); // Initially hide the border by setting alpha to 0
 
 		const alphaBeta = this.add
-			.text(450, 305, "AlphaBeta", {
-				...this.fontStyle,
-				fontWeight: 'bold' // Add fontWeight to the font style
-			})
+			.text(450, 305, "AlphaBeta", this.fontStyle)
 			.setOrigin(0.5, 0.5)
 			.setInteractive({ useHandCursor: true })
 			.on("pointerdown", () => {
@@ -240,7 +244,7 @@ export default class StartScene extends Phaser.Scene {
 					targets: alphaBetaGraphics,
 					alpha: 1,
 					duration: 500, // Duration of the fade-in effect in milliseconds
-					ease: 'Power2'
+					ease: "Power2",
 				});
 			})
 			.on("pointerout", () => {
@@ -248,14 +252,14 @@ export default class StartScene extends Phaser.Scene {
 					targets: alphaBetaGraphics,
 					alpha: 0,
 					duration: 500, // Duration of the fade-out effect in milliseconds
-					ease: 'Power2'
+					ease: "Power2",
 				});
 			})
 			.setVisible(false);
 
 		const easyGraphics = this.add.graphics();
 		easyGraphics.lineStyle(2, 0x000000, 1); // Set border color to black and thickness to 2px
-		easyGraphics.fillStyle(0x3DD1E7, 1); // Set background color to red
+		easyGraphics.fillStyle(0x3dd1e7, 1); // Set background color to red
 		easyGraphics.strokeRoundedRect(350, 185, 200, 50, 10); // Draw rounded rectangle border with border radius 10px
 		easyGraphics.fillRoundedRect(350, 185, 200, 50, 10); // Fill rounded rectangle with border radius 10px
 		easyGraphics.setAlpha(0); // Initially hide the border by setting alpha to 0
@@ -272,7 +276,7 @@ export default class StartScene extends Phaser.Scene {
 					targets: easyGraphics,
 					alpha: 1,
 					duration: 500, // Duration of the fade-in effect in milliseconds
-					ease: 'Power2'
+					ease: "Power2",
 				});
 			})
 			.on("pointerout", () => {
@@ -280,14 +284,14 @@ export default class StartScene extends Phaser.Scene {
 					targets: easyGraphics,
 					alpha: 0,
 					duration: 500, // Duration of the fade-out effect in milliseconds
-					ease: 'Power2'
+					ease: "Power2",
 				});
 			})
 			.setVisible(false);
 
 		const mediumGraphics = this.add.graphics();
 		mediumGraphics.lineStyle(2, 0x000000, 1); // Set border color to black and thickness to 2px
-		mediumGraphics.fillStyle(0x3DD1E7, 1); // Set background color to red
+		mediumGraphics.fillStyle(0x3dd1e7, 1); // Set background color to red
 		mediumGraphics.strokeRoundedRect(350, 285, 200, 50, 10); // Draw rounded rectangle border with border radius 10px
 		mediumGraphics.fillRoundedRect(350, 285, 200, 50, 10); // Fill rounded rectangle with border radius 10px
 		mediumGraphics.setAlpha(0); // Initially hide the border by setting alpha to 0
@@ -304,7 +308,7 @@ export default class StartScene extends Phaser.Scene {
 					targets: mediumGraphics,
 					alpha: 1,
 					duration: 500, // Duration of the fade-in effect in milliseconds
-					ease: 'Power2'
+					ease: "Power2",
 				});
 			})
 			.on("pointerout", () => {
@@ -312,14 +316,14 @@ export default class StartScene extends Phaser.Scene {
 					targets: mediumGraphics,
 					alpha: 0,
 					duration: 500, // Duration of the fade-out effect in milliseconds
-					ease: 'Power2'
+					ease: "Power2",
 				});
 			})
 			.setVisible(false);
 
 		const hardGraphics = this.add.graphics();
 		hardGraphics.lineStyle(2, 0x000000, 1); // Set border color to black and thickness to 2px
-		hardGraphics.fillStyle(0x3DD1E7, 1); // Set background color to red
+		hardGraphics.fillStyle(0x3dd1e7, 1); // Set background color to red
 		hardGraphics.strokeRoundedRect(350, 385, 200, 50, 10); // Draw rounded rectangle border with border radius 10px
 		hardGraphics.fillRoundedRect(350, 385, 200, 50, 10); // Fill rounded rectangle with border radius 10px
 		hardGraphics.setAlpha(0); // Initially hide the border by setting alpha to 0
@@ -336,7 +340,7 @@ export default class StartScene extends Phaser.Scene {
 					targets: hardGraphics,
 					alpha: 1,
 					duration: 500, // Duration of the fade-in effect in milliseconds
-					ease: 'Power2'
+					ease: "Power2",
 				});
 			})
 			.on("pointerout", () => {
@@ -344,14 +348,14 @@ export default class StartScene extends Phaser.Scene {
 					targets: hardGraphics,
 					alpha: 0,
 					duration: 500, // Duration of the fade-out effect in milliseconds
-					ease: 'Power2'
+					ease: "Power2",
 				});
 			})
 			.setVisible(false);
 
 		const backGraphics = this.add.graphics();
-		backGraphics.lineStyle(2, 0xAE445A, 1); // Set border color to white and thickness to 2px
-		backGraphics.fillStyle(0xAE445A, 1); // Set background color to red
+		backGraphics.lineStyle(2, 0xae445a, 1); // Set border color to white and thickness to 2px
+		backGraphics.fillStyle(0xae445a, 1); // Set background color to red
 		backGraphics.strokeRoundedRect(350, 485, 200, 50, 10); // Draw rounded rectangle border with border radius 10px
 		backGraphics.fillRoundedRect(350, 485, 200, 50, 10); // Fill rounded rectangle with border radius 10px
 		backGraphics.setAlpha(0); // Initially hide the border by setting alpha to 0
@@ -359,7 +363,7 @@ export default class StartScene extends Phaser.Scene {
 		const back = this.add
 			.text(450, 507, "↩ Back", {
 				...this.fontStyle,
-				fill: "#FFFFFF" // Set text color to white
+				fill: "#FFFFFF", // Set text color to white
 			})
 			.setOrigin(0.5, 0.5)
 			.setInteractive({ useHandCursor: true })
@@ -378,15 +382,12 @@ export default class StartScene extends Phaser.Scene {
 				algorithm.setVisible(false); // Hide "Thuật toán"
 				menu.setVisible(true); // Show "Menu"
 				level.setVisible(false);
-				
 			})
 			.setVisible(false); // Initially hide the text
-
-
 	}
 
 	/**
 	 * Update the scene. This method is called every frame.
 	 */
-	update() { }
+	update() {}
 }
